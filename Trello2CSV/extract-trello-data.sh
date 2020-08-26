f8ae967c4efa1b2502c81c884fa2080992f42982
David Coomber <47242934+dcoomber@users.noreply.github.com>
2020-08-26 21:38:05 +0200

    No more Excel for looking up related data!  Yay!

--- Trello2CSV/extract-trello-data.sh
+++ Trello2CSV/extract-trello-data.sh
@@ -2,30 +2,18 @@
 
 INPUT=$1
 
-OUT_ACTIONS=data_actions.csv
-OUT_CARDS=data_cards.csv
-OUT_LABELS=data_labels.csv
-OUT_LISTS=data_lists.csv
-OUT_MEMBERS=data_members.csv
+OUTPUT=data_cards.csv
 
 # TODO: Automate download of Trello board
 
-# extract action detail
-echo type,date,board_name,list_name,list_before,list_after,action_by > ${OUT_ACTIONS}
-cat ${INPUT} | jq -r '.actions[] | [.type, .date, .data.board.name, .data.list.name, .data.listBefore.name, .data.listAfter.name, .memberCreator.fullName] | @csv' >> ${OUT_ACTIONS}
-
-# extract the cards detail
-echo id,item,background,short_id,short_url,list,label1,label2,label3,label4,label5,archived > ${OUT_CARDS}
-cat ${INPUT} | jq -r '.cards[] | [.id, .name, .desc, .idShort, .shortUrl, .idList, .idLabels[0], .idLabels[1], .idLabels[2], .idLabels[3], .idLabels[4], .closed] | @csv' >> ${OUT_CARDS}
-
-# extract labels detail
-echo id,name > ${OUT_LABELS}
-cat ${INPUT} | jq -r '.labels[] | [.id, .name] | @csv' >> ${OUT_LABELS}
-
-# extract lists detail
-echo id,name,archived > ${OUT_LISTS}
-cat ${INPUT} | jq -r '.lists[] | [.id, .name, .closed] | @csv' >> ${OUT_LISTS}
-
-# extract members detail
-echo id,name > ${OUT_MEMBERS}
-cat ${INPUT} | jq -r '.members[] | [.id, .fullName] | @csv' >> ${OUT_MEMBERS}
+# Retrieve card details, lookup list and label details
+# Special thanks to https://gist.github.com/mgeh/8644a15a96759921505fd531a0cb8f2f
+cat ${INPUT} | jq -r '
+    (
+        ["Short ID", "Item", "Background", "Short URL", "Card Closed", "List", "List Closed", "Labels"], 
+        (reduce .lists[] as $listName ({}; .[$listName.id] = $listName.name)) as $listNames | 
+        (reduce .lists[] as $listStatus ({}; .[$listStatus.id] = $listStatus.closed)) as $listStatuses | 
+        (reduce .labels[] as $labelName ({}; .[$labelName.id] = $labelName.name)) as $labelNames | 
+        .cards[] | 
+        [.idShort, .name, .desc, .shortUrl, .closed, $listNames[.idList], $listStatuses[.idList], $listStatuses[.labels[].id]]
+    ) | @csv' > ${OUTPUT}
