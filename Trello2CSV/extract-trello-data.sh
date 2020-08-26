7b2998e0e09e41ce95912ed4381110fe6b39dcd9
David Coomber <47242934+dcoomber@users.noreply.github.com>
2020-08-26 21:45:42 +0200

    No more Excel for looking up related data!  Yay!

--- Trello2CSV/extract-trello-data.sh
+++ Trello2CSV/extract-trello-data.sh
@@ -10,10 +10,10 @@ OUTPUT=data_cards.csv
 # Special thanks to https://gist.github.com/mgeh/8644a15a96759921505fd531a0cb8f2f
 cat ${INPUT} | jq -r '
     (
-        ["Short ID", "Item", "Background", "Short URL", "List", "Card Closed", "List Closed", "Labels"], 
+        ["Short ID", "Item", "Background", "Short URL", "List", "Card Closed", "List Closed", "Label 1", "Label 2", "Label 3", "Label 4", "Label 5"], 
         (reduce .lists[] as $listName ({}; .[$listName.id] = $listName.name)) as $listNames | 
         (reduce .lists[] as $listStatus ({}; .[$listStatus.id] = $listStatus.closed)) as $listStatuses | 
         (reduce .labels[] as $labelName ({}; .[$labelName.id] = $labelName.name)) as $labelNames | 
         .cards[] | 
-        [.idShort, .name, .desc, .shortUrl, $listNames[.idList], .closed, $listStatuses[.idList], $listStatuses[.labels[].id]]
+        [.idShort, .name, .desc, .shortUrl, $listNames[.idList], .closed, $listStatuses[.idList], $labelNames[.labels[].id]]
     ) | @csv' > ${OUTPUT}
