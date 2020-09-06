3db2a8882bacd2ebe28b30f2cea1b747a979eaf8
David Coomber <47242934+dcoomber@users.noreply.github.com>
2020-09-06 21:41:02 +0200

    Fixed based on shellcheck static analysis (#9)
    * Fixed based on shellcheck static analysis
    
    * Create pullrequest.yml

--- Installers/setup-sweethome3d.sh
+++ Installers/setup-sweethome3d.sh
@@ -15,71 +15,77 @@ RED='\033[1;31m'    # Brown/Orange
 YELLOW='\033[1;33m' # Yellow
 NC='\033[0m'        # No Color
 
-printf "${RED}Installing '$APP_NAME'${NC}\n"
+printf "${RED}Installing '%s'${NC}\n" "${APP_NAME}"
 
 # Ensure working directory exists
-if [ ! -d "$APP_NAME" ]; then
-    printf "${YELLOW}Creating directory '$APP_NAME'.${NC}\n"
-    mkdir $APP_NAME || exit 1
+if [ ! -d "${APP_NAME}" ]; then
+    printf "${YELLOW}Creating directory '%s'.${NC}\n" "${APP_NAME}"
+    mkdir ${APP_NAME} || exit 1
 fi
 
 # Move to working directory
-pushd "$APP_NAME" > /dev/null
+pushd "${APP_NAME}" > /dev/null || exit
 
 # Download the archive if it doesn't already exist
-if [ ! -f "$FILE_NAME" ]; then
-    printf "${YELLOW}Downloading archive '$FILE_NAME'.${NC}\n"
+if [ ! -f "${FILE_NAME}" ]; then
+    printf "${YELLOW}Downloading archive '%s'.${NC}\n" "${FILE_NAME}"
     wget $DOWNLOAD_URL || exit 1
 fi
 
 # Uncompress the archive
-printf "${YELLOW}Expanding archive '$FILE_NAME'.${NC}\n"
-if [ -f "$FILE_NAME" ]; then
-    tar -zxvf $FILE_NAME > /dev/null || exit 1
+printf "${YELLOW}Expanding archive '%s'.${NC}\n" "${FILE_NAME}"
+if [ -f "${FILE_NAME}" ]; then
+    tar -zxvf "${FILE_NAME}" > /dev/null || exit 1
 else
+    # shellcheck disable=SC2059
     printf "${RED}Could not find the downloaded archive.${NC}\n"
     exit 1
 fi
 
 # Move the archive to opt directory
 if [ -d "${APP_NAME}-${APP_VER}" ]; then
-    printf "${YELLOW}Moving '${APP_NAME}-${APP_VER}' to /opt.${NC}\n"
+    printf "${YELLOW}Moving '%s-%s' to /opt.${NC}\n" "${APP_NAME}" "${APP_VER}"
 
     # Delete the target directory
     sudo rm -rf "/opt/${APP_NAME}-${APP_VER}"
     
     # Move the expanded directory
-    sudo mv "${APP_NAME}-${APP_VER}" /opt/ && sudo chown -R ${USER}:${USER} "/opt/${APP_NAME}-${APP_VER}"
+    sudo mv "${APP_NAME}-${APP_VER}" /opt/ && sudo chown -R "${USER}":"${USER}" "/opt/${APP_NAME}-${APP_VER}"
 
     # Copy the icon for later use
     sudo cp "${ICON_DIR_SOURCE}" "${ICON_DIR_TARGET}"
 else
+    # shellcheck disable=SC2059
     printf "${RED}Something went wrong when expanding the archive.${NC}\n"
     exit 1
 fi
 
 # Create launcher to menu
+# shellcheck disable=SC2059
 printf "${YELLOW}Creating launcher on your desktop.${NC}\n"
 rm -rf "${LAUNCHER}"
 touch "${LAUNCHER}" && chmod +x "${LAUNCHER}"
 
-echo "[Desktop Entry]" >> "${LAUNCHER}"
-echo "Version=1.0" >> "${LAUNCHER}"
-echo "Name=${APP_NAME} ${APP_VER}" >> "${LAUNCHER}"
-echo "GenericName=Interior 2D design application with 3D preview" >> "${LAUNCHER}"
-echo "GenericName[de]=Innenraumplaner" >> "${LAUNCHER}"
-echo "Comment=Interior design Java application for quickly choosing and placing furniture on a house 2D plan drawn by the end-user with a 3D preview" >> "${LAUNCHER}"
-echo "Exec=/opt/${APP_NAME}-${APP_VER}/${APP_NAME}" >> "${LAUNCHER}"
-echo "Icon=${ICON_DIR_TARGET}" >> "${LAUNCHER}"
-echo "StartupNotify=true" >> "${LAUNCHER}"
-echo "StartupWMClass=com-eteks-sweethome3d-SweetHome3D" >> "${LAUNCHER}"
-echo "Terminal=false" >> "${LAUNCHER}"
-echo "Type=Application" >> "${LAUNCHER}"
-echo "Categories=Graphics;2DGraphics;3DGraphics;" >> "${LAUNCHER}"
-echo "Keywords=interior;design;2D;3D;home;house;furniture;java;" >> "${LAUNCHER}"
+{
+    echo "[Desktop Entry]"
+    echo "Version=1.0"
+    echo "Name=${APP_NAME} ${APP_VER}"
+    echo "GenericName=Interior 2D design application with 3D preview"
+    echo "GenericName[de]=Innenraumplaner"
+    echo "Comment=Interior design Java application for quickly choosing and placing furniture on a house 2D plan drawn by the end-user with a 3D preview"
+    echo "Exec=/opt/${APP_NAME}-${APP_VER}/${APP_NAME}"
+    echo "Icon=${ICON_DIR_TARGET}"
+    echo "StartupNotify=true"
+    echo "StartupWMClass=com-eteks-sweethome3d-SweetHome3D"
+    echo "Terminal=false"
+    echo "Type=Application"
+    echo "Categories=Graphics;2DGraphics;3DGraphics;"
+    echo "Keywords=interior;design;2D;3D;home;house;furniture;java;"
+} > "${LAUNCHER}"
 
 # cp launcher to menu
+# shellcheck disable=SC2059
 printf "${YELLOW}Copying desktop launcher to system menu.${NC}\n"
 sudo cp "${LAUNCHER}" "/usr/share/applications/"
 
-popd > /dev/null
+popd > /dev/null || exit
