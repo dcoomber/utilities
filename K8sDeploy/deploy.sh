20fc62100a2c5004f01320894136f99ef06ec779
David Coomber <47242934+dcoomber@users.noreply.github.com>
2020-09-06 20:31:40 +0200

    Fixed based on shellcheck static analysis

--- K8sDeploy/deploy.sh
+++ K8sDeploy/deploy.sh
@@ -32,124 +32,127 @@ config="$config_dir/$1"
 # validate configuration
 if [ ! -f "$defaults" ]; then
     echo
-    printf "${ERROR}Configuration file not found at '$defaults'.${NC}\n"
+    printf "${ERROR}Configuration file not found at '%s'.${NC}\n" "${defaults}"
     usage
 fi
 
 if [ ! -f "$config" ]; then
     echo
-    printf "${ERROR}Configuration file not found at '$config'.${NC}\n"
+    printf "${ERROR}Configuration file not found at '%s'${NC}\n" "$config"
     usage
 fi
 
 # load configuration
-source "$defaults"
-source "$config"
+# shellcheck source=config/defaults
+source "${defaults}"
+
+# shellcheck source=config/uat
+source "${config}"
 
 # work in source repo directory
-pushd $PWD > /dev/null
+pushd "$PWD" > /dev/null || exit
 
 # set Dockerfile path
 if [ "$2" == "" ]; then
     dockerfile=Dockerfile
-    app=$APP
+    app=${app}
 else
     dockerfile="$2/Dockerfile"
     app=$2
 fi
 
 # directory must contain a Dockerfile
-if [ ! -f "$dockerfile" ]; then
-    printf "${ERROR}Dockerfile doesn't exist in the specified directory:${NC} $dockerfile\n"
+if [ ! -f "${dockerfile}" ]; then
+    printf "${ERROR}Dockerfile doesn't exist in the specified directory:${NC} %s\n" "${dockerfile}"
     usage
 fi
 
 # must be run from within a git repo directory
 if ! git status; then
+    # shellcheck disable=SC2059
     printf "${ERROR}This script must be executed from within a valid git repo clone.${NC}\n"
     usage
 fi
 
 # pull local branch (update)
 if ! git pull; then
-    printf "${ERROR}Could not pull brach.  See previous errors.${NC}\n"
+    # shellcheck disable=SC2059
+    printf "${ERROR}Could not pull branch.  See previous errors.${NC}\n"
     usage
 fi
 
 # build the image
-IMAGE_PATH="$IMAGE_PATH_ROOT/$app:$BRANCH"
-echo
-printf "${WARNING}Building image ($IMAGE_PATH)${NC}\n"
+IMAGE_PATH="${IMAGE_PATH}_ROOT/${app}:${BRANCH}"
+printf "\n${WARNING}Building image (%s)${NC}\n" "${IMAGE_PATH}"
 
-echo "  docker build -t $IMAGE_PATH -f $dockerfile ."
-echo "    or"
-echo "  docker build -t $IMAGE_PATH -f $dockerfile --no-cache ."
-if ! docker build -t $IMAGE_PATH -f $dockerfile --no-cache .; then
-    echo
-    printf "${ERROR}Building image failed.  See above for details.${NC}\n"
-    echo
+printf "  docker build -t %s -f %s .\n" "${IMAGE_PATH}" "${dockerfile}"
+printf "    or\n"
+printf "  docker build -t %s -f %s --no-cache .\n" "${IMAGE_PATH}" "${dockerfile}"
+
+if ! docker build -t "${IMAGE_PATH}" -f "${dockerfile}" --no-cache .; then
+    # shellcheck disable=SC2059
+    printf "\n${ERROR}Building image failed.  See above for details.${NC}\n\n"
     exit 1
 fi
 
 # push the image
-echo
-printf "${WARNING}Pushing image${NC}\n"
-echo "docker push $IMAGE_PATH"
+# shellcheck disable=SC2059
+printf "\n${WARNING}Pushing image${NC}\n"
+printf "docker push %s\n" "${IMAGE_PATH}"
 
-if ! docker push $IMAGE_PATH; then
-    echo
-    printf "${ERROR}Pushing image failed.  See above for details.${NC}\n"
-    echo
+if ! docker push "${IMAGE_PATH}"; then
+    # shellcheck disable=SC2059
+    printf "\n${ERROR}Pushing image failed.  See above for details.${NC}\n\n"
     exit 1
 fi
 
 # delete pod to force deployment of new image
-echo
-printf "${WARNING}Deleting pods${NC}\n"
+# shellcheck disable=SC2059
+printf "\n${WARNING}Deleting pods${NC}\n"
 
-if ! kubectl --context $CONTEXT -n $NAMESPACE delete pods -l app=$app; then
-    echo
-    printf "${ERROR}Deleting pods failed.  See above for details.${NC}\n"
-    echo
+if ! kubectl --context "${CONTEXT}" -n "${NAMESPACE}" delete pods -l app="${app}"; then
+    # shellcheck disable=SC2059
+    printf "\n${ERROR}Deleting pods failed.  See above for details.${NC}\n\n"
     exit 1
 fi
 
+# shellcheck disable=SC2059
 printf "${WARNING}To delete pods${NC}\n"
-echo "  kubectl --context $CONTEXT -n $NAMESPACE delete pods -l app=$app"
+printf "  kubectl --context %s -n %s delete pods -l app=%s\n\n" "${CONTEXT}" "${NAMESPACE}" "${app}"
 
 # view status
-echo
+# shellcheck disable=SC2059
 printf "${WARNING}To monitor pod start-up:${NC}\n"
-echo "  kubectl --context $CONTEXT -n $NAMESPACE get pods -w"
-echo "    or"
-echo "  kubectl --context $CONTEXT -n $NAMESPACE get pods -w | grep $app"
+printf "  kubectl --context %s -n %s get pods -w\n" "${CONTEXT}" "${NAMESPACE}"
+printf "    or\n"
+printf "  kubectl --context %s -n %s get pods -w | grep %s\n\n" "${CONTEXT}" "${NAMESPACE}" "${app}"
 
-echo
+# shellcheck disable=SC2059
 printf "${WARNING}To view ingress details:${NC}\n"
-echo "  kubectl --context $CONTEXT get ing -n $NAMESPACE"
-echo "    or"
-echo "  kubectl --context $CONTEXT get ing -n $NAMESPACE | grep $app"
+printf "  kubectl --context %s get ing -n %s\n" "${CONTEXT}" "${NAMESPACE}"
+printf "    or\n"
+printf "  kubectl --context %s get ing -n %s | grep %s\n\n" "${CONTEXT}" "${NAMESPACE}" "${app}"
 
-echo
+# shellcheck disable=SC2059
 printf "${WARNING}To change helm installation (update the <<HOST>> in the command below)${NC}\n"
-printf "  helm --kube-context $CONTEXT --namespace $NAMESPACE upgrade --force --install $app-$BRANCH --set image.tag=$BRANCH,ingress.vhost=${WARNING}<<HOST>>${NC}.$NAMESPACE.env charts/$APP\n"
+printf "  helm --kube-context %s --namespace %s upgrade --force --install %s-%s --set image.tag=%s,ingress.vhost=${WARNING}<<HOST>>${NC}.%s.env charts/%s\n\n" "${CONTEXT}" "${NAMESPACE}" "${app}" "${BRANCH}" "${BRANCH}" "${NAMESPACE}" "${app}"
 
-echo
+# shellcheck disable=SC2059
 printf "${WARNING}To check your deployment config:${NC}\n"
-# $NAMESPACE must be included in metadata/namespace AND metadata/selfLink
-# $BRANCH must be inclued in spec/template/spec/image
-echo "  kubectl --context $CONTEXT -n $NAMESPACE edit deployment $app"
+# ${NAMESPACE} must be included in metadata/namespace AND metadata/selfLink
+# ${BRANCH} must be inclued in spec/template/spec/image
+printf "  kubectl --context %s -n %s edit deployment %s\n\n" "${CONTEXT}" "${NAMESPACE}" "${app}"
 
-echo
+# shellcheck disable=SC2059
 printf "${WARNING}To view application logs:${NC}\n"
-printf "  stern --context $CONTEXT --namespace $NAMESPACE -l app=$app --tail 100 | grep ${WARNING}<<search term>>${NC}\n"
+printf "  stern --context %s --namespace %s -l app=%s --tail 100 | grep ${WARNING}<<search term>>${NC}\n\n" "${CONTEXT}" "${NAMESPACE}" "${app}" "${CONTEXT}"
 
-echo
+# shellcheck disable=SC2059
 printf "${WARNING}To edit your namespace:${NC}\n"
-echo "  kubectl --context $CONTEXT edit ns $NAMESPACE"
+printf "  kubectl --context %s edit ns %s\n\n" "${CONTEXT}" "${NAMESPACE}"
 
-echo
+# shellcheck disable=SC2059
 printf "${WARNING}To view pod details and events:${NC}\n"
-printf "  kubectl --context $CONTEXT describe pods -n $NAMESPACE ${WARNING}<<pod_name>>${NC}\n"
+printf "  kubectl --context %s describe pods -n %s ${WARNING}<<pod_name>>${NC}\n" "${CONTEXT}" "${NAMESPACE}"
 
-popd > /dev/null
+popd > /dev/null || exit
